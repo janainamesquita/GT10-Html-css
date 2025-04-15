53daf176eccab2f47b7e53a5f8ba3f2f73fe1a5c
janainamesquita <janainammesquita@hotmail.com>
2025-04-15 11:57:26 -0300

    Aula7

---
+++ javascript/atividade-15.js
@@ -0,0 +1,44 @@
+const user = {
+    nome: "Janaína",
+    altura: 1.74,
+    peso: 70
+}
+
+//criar uma função que receba o objeto user
+// e calcule o IMC
+// IMC = peso / (altura * altura)
+
+function calcularIMC(user){
+    const imc = user.peso / (user.altura * user.altura)
+
+    if(imc < 16){
+        return console.log("Magreza grave")
+    } 
+    
+    if (imc >= 16 && imc < 17){
+        return console.log("Magreza moderada")
+    } 
+    
+    if (imc >= 17 && imc < 18.5) { 
+        return console.log("Magreza leve")
+    }
+    
+    if (imc >= 18.6 && imc < 24.9) {
+        return console.log("Normal")
+    } 
+    
+    if (imc >= 25 && imc < 29.9) {
+        return console.log("Sobrepeso")
+    } 
+    
+    if (imc >= 30 && imc < 34.9) {
+        return console.log("Obesidade de grau I")
+    } 
+        
+    if (imc >= 35 && imc < 39.9) {
+        return console.log("Obesidade")
+    }
+    }
+
+
+calcularIMC(user)
